8bc03dc20a9a3f9982b27b29e72086f0e4adb799
Josue Silva de Morais <56761970+josuemoraisgh@users.noreply.github.com>
2024-05-26 17:33:28 -0300

    refactor btn

--- src/main.cpp
+++ src/main.cpp
@@ -26,14 +26,22 @@ void loop()
   InIndKit.update();
   if (debounceBtn(&InIndKit.rtn_1)) // Checa se o botao alterou sem debounce e se sim mudar o seu valor de status
     if (InIndKit.rtn_1.status_btn) // Se o valor do status for verdadeiro
-      Telnet.print("BotaoRTN1 Ativo");
+      Telnet.println("BotaoRTN1 ON");
+    else
+      Telnet.println("BotaoRTN1 OFF");
   if (debounceBtn(&InIndKit.rtn_2))
     if (InIndKit.rtn_2.status_btn)
-      Telnet.print("BotaoRTN2 Ativo");
+      Telnet.println("BotaoRTN2 ON");
+    else
+      Telnet.println("BotaoRTN2 OFF");      
   if (debounceBtn(&InIndKit.push_1))
     if (InIndKit.push_1.status_btn)
-      Telnet.print("BotaoPUSH1 Ativo");
+      Telnet.println("BotaoPUSH1 ON");
+    else
+      Telnet.println("BotaoPUSH1 OFF");      
   if (debounceBtn(&InIndKit.push_2))
     if (InIndKit.push_2.status_btn)
-      Telnet.print("BotaoPUSH2 Ativo");
+      Telnet.println("BotaoPUSH2 ON");
+    else
+      Telnet.println("BotaoPUSH2 OFF");      
 }

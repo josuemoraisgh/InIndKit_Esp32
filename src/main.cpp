fa274302d3794ff5441a2233341d2fdcd02910ce
Josue Silva de Morais <56761970+josuemoraisgh@users.noreply.github.com>
2024-06-09 17:46:28 -0300

    refactor main.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 #include "InIndKit.h"
 
-AsyncDelay_c delayPlot(50); // time mili second
-float timeStampsPlot = 0.0;
-void printPlot(void);
-
 AsyncDelay_c delayPOT(500); // time mili second
 void monitoraPOT(void);
-
-void telnetRead(String str);
 
 void setup()
 {
   InIndKit.setup("APJosue", "josue32154538"); //("Wokwi-GUEST","");// ssid, password ;
-  Telnet.onInputReceived(telnetRead);
+  Telnet.onInputReceived([](String str) 
+  {
+      if (str == "ping") // checks for a certain command
+      {
+        Telnet.println("pong");
+        Serial.println("- Telnet: pong");
+      }
+      else if (str == "bye") // disconnect the client
+      {
+        Telnet.println("disconnecting you...");
+        Telnet.disconnectClient();
+      }
+      else
+      {
+        Telnet.println(str);
+      } 
+  });
+  
   attachInterrupt(
       InIndKit.rtn_1.pin,
       []()
@@ -21,56 +32,37 @@ void setup()
         if (debounceBtn(&InIndKit.rtn_1)) // Checa se o botao alterou sem debounce e se sim mudar o seu valor de status
           Telnet.println(InIndKit.rtn_1.status_btn ? "BotaoRTN1 ON" : "BotaoRTN1 OFF");
       },
-      RISING);
+      CHANGE);
   attachInterrupt(
       InIndKit.rtn_2.pin,
       []()
       {
         if (debounceBtn(&InIndKit.rtn_2)) // Checa se o botao alterou sem debounce e se sim mudar o seu valor de status
-          Telnet.println(InIndKit.rtn_2.status_btn ? "BotaoRTN1 ON" : "BotaoRTN1 OFF");
+          Telnet.println(InIndKit.rtn_2.status_btn ? "BotaoRTN2 ON" : "BotaoRTN2 OFF");
       },
-      RISING);
+      CHANGE);
   attachInterrupt(
       InIndKit.push_1.pin,
       []()
       {
         if (debounceBtn(&InIndKit.push_1)) // Checa se o botao alterou sem debounce e se sim mudar o seu valor de status
-          Telnet.println(InIndKit.push_1.status_btn ? "BotaoRTN1 ON" : "BotaoRTN1 OFF");
+          Telnet.println(InIndKit.push_1.status_btn ? "BotaoPush_1 ON" : "BotaoPush_1 OFF");
       },
-      RISING);
+      CHANGE);
   attachInterrupt(
       InIndKit.push_2.pin,
       []()
       {
         if (debounceBtn(&InIndKit.push_2)) // Checa se o botao alterou sem debounce e se sim mudar o seu valor de status
-          Telnet.println(InIndKit.push_2.status_btn ? "BotaoRTN1 ON" : "BotaoRTN1 OFF");
+          Telnet.println(InIndKit.push_2.status_btn ? "BotaoPush_2 ON" : "BotaoPush_2 OFF");
       },
-      RISING);                  
+      CHANGE);
 }
 
 void loop()
 {
   InIndKit.loop();
   monitoraPOT();
-  printPlot();
-}
-
-void telnetRead(String str)
-{
-  if (str == "ping") // checks for a certain command
-  {
-    Telnet.println("pong");
-    Serial.println("- Telnet: pong");
-  }
-  else if (str == "bye") // disconnect the client
-  {
-    Telnet.println("disconnecting you...");
-    Telnet.disconnectClient();
-  }
-  else
-  {
-    Telnet.println(str);
-  }
 }
 
 void monitoraPOT(void)
@@ -81,21 +73,10 @@ void monitoraPOT(void)
 
     const uint16_t vlPOT_LEFT = analogRead(def_pin_POT_LEFT);
     InIndKit.setDisplayText(2, String(vlPOT_LEFT).c_str());
-    Telnet.plot("pot_LEFT", vlPOT_LEFT, "V");
+    Telnet.plot("pot_LEFT", vlPOT_LEFT);
 
     const uint16_t vlPOT_RIGHT = analogRead(def_pin_POT_RIGHT);
     InIndKit.setDisplayText(3, String(vlPOT_RIGHT).c_str());
-    Telnet.plot("pot_RIGHT", vlPOT_RIGHT, "V");
-  }
-}
-
-void printPlot(void)
-{
-  if (delayPlot.isExpired())
-  {
-    delayPlot.repeat();
-    timeStampsPlot += 0.1;
-    Telnet.plot("sin", timeStampsPlot, sin(timeStampsPlot)); // Plot a sinus
-    Telnet.plot("cos", timeStampsPlot, cos(timeStampsPlot)); // Plot a cosinus
+    Telnet.plot("pot_RIGHT", vlPOT_RIGHT);
   }
 }

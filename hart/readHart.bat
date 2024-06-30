45d65d7b728fd81e57f0da350cdee74b2f2bd809
Josue Silva de Morais <56761970+josuemoraisgh@users.noreply.github.com>
2024-06-30 06:01:47 -0300

    refactor inindkit

--- hart/readHart.bat
+++ hart/readHart.bat
@@ -1,4 +1,5 @@
 @echo off
 set kitId=0
+set LIBSERIALPORT_PATH=.
 CALL udpserial.exe inindkit%kitId%.local 400%kitId% CNCA%kitId%
 rem for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:"IPv4 Address"`) do echo ip:%%f port:%port%  | ncat -w10 inindkit%kitId%.local 4000
